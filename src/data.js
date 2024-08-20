fba94f530c56ddb16c2d94e7bfb26a1e67933569
James Mccarthy <mccarthyjames35@yahoo.com>
2024-08-20 12:38:38 -0700

    running build

---
+++ src/data.js
@@ -0,0 +1,135 @@
+import tourImg1 from './images/tour-1.jpeg';
+import tourImg2 from './images/tour-2.jpeg';
+import tourImg3 from './images/tour-3.jpeg';
+import tourImg4 from './images/tour-4.jpeg';
+import tourImg5 from './images/tour-5.jpeg';
+import tourImg6 from './images/tour-6.jpeg';
+
+export const pageLinks = [
+	{
+		id: 1,
+		href: '#home',
+		text: 'home'
+	},
+	{
+		id: 2,
+		href: '#about',
+		text: 'about'
+	},
+	{
+		id: 3,
+		href: '#services',
+		text: 'services'
+	},
+	{
+		id: 4,
+		href: '#tours',
+		text: 'tours'
+	}
+];
+
+export const socialLinks = [
+	{
+		id: 1,
+		href: 'https://www.facebook.com',
+		icon: 'fab fa-facebook'
+	},
+	{
+		id: 2,
+		href: 'https://www.twitter.com',
+		icon: 'fab fa-twitter'
+	},
+	{
+		id: 3,
+		href: 'https://www.squarespace.com',
+		icon: 'fab fa-squarespace'
+	}
+];
+
+export const services = [
+	{
+		id: 1,
+		icon: 'fas fa-wallet fa-fw',
+		title: 'saving money',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+	},
+
+	{
+		id: 2,
+		icon: 'fas fa-tree fa-fw',
+		title: 'endless hiking',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+	},
+
+	{
+		id: 3,
+		icon: 'fas fa-socks fa-fw',
+		title: 'amazing comfort',
+		text: 'Lorem ipsouu  dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+	}
+];
+
+export const tours = [
+	{
+		id: 1,
+		image: tourImg1,
+		date: 'January 1st, 2020',
+		title: 'Tibet Adventure',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis. Eum et deleniti autempeaque ipsum.',
+		location: 'china',
+		duration: '6 days',
+		cost: 'From $2100'
+	},
+	{
+		id: 2,
+		image: tourImg2,
+		date: 'October 1th, 2020',
+		title: 'Best of Java',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis. Eum et deleniti autempeaque ipsum.',
+		location: 'china',
+		duration: '11 days',
+		cost: 'From $1400'
+	},
+	{
+		id: 3,
+		image: tourImg3,
+		date: 'September 15th, 2020',
+		title: 'Explore Hong Kong',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis. Eum et deleniti autempeaque ipsum.',
+		location: 'Hong Kong',
+		duration: '8 days',
+		cost: 'From $5000'
+	},
+	{
+		id: 4,
+		image: tourImg4,
+		date: 'december 5th, 2019',
+		title: 'Kenya Highlights',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis. Eum et deleniti autempeaque ipsum.',
+		location: 'Kenya',
+		duration: '20 days',
+		cost: 'From $ 3300'
+	},
+
+	{
+		id: 5,
+		image: tourImg5,
+		date: 'august 26th, 2020',
+		title: 'Kingdom of Bhutan',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis. Eum et deleniti autempeaque ipsum.',
+		location: ' Bhutan',
+		duration: '16 days',
+		cost: 'From $1400'
+	},
+
+	{
+		id: 6,
+		image: tourImg6,
+		date: 'august 26th, 2020',
+		title: 'Kashmir Adventure',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis. Eum et deleniti autempeaque ipsum.',
+		location: 'Kashmir',
+		duration: '12 days',
+		cost: 'From $2100'
+	}
+];
